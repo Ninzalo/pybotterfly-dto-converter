ce2d8835a1d832b4dac79cfde85d77c78a84efec
Ninzalo <k-artemiy@mail.ru>
2024-04-13 21:21:36 +0300

    refactor: add docstring to BaseDTOConverter

--- pybotterfly_converter/BaseDTOConverter.py
+++ pybotterfly_converter/BaseDTOConverter.py
@@ -9,14 +9,28 @@ class IsDataclass(Protocol):
 
 
 class BaseDTOConverter(ABC):
-    """Converter interface"""
+    """DTO Converter interface"""
 
     @classmethod
     @abstractmethod
     async def encode_dto_to_bytes(cls, dto: IsDataclass) -> bytes:
-        """Encode DTO to bytes"""
+        """Encode DTO to bytes
+
+        :param dto: The DTO to convert.
+        :type dto: IsDataclass
+
+        :return: bytes
+        :rtype: bytes
+        """
 
     @classmethod
     @abstractmethod
     async def decode_bytes_to_dto(cls, data: bytes) -> IsDataclass:
-        """Decode bytes to DTO"""
+        """Decode bytes to DTO
+
+        :param data: The bytes to convert to DTO.
+        :type data: bytes
+
+        :return: The DTO
+        :rtype: IsDataclass
+        """
